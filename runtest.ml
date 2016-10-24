62e9b3e68e178347cc00c2916e1100660e64f42b
Michael Greenberg <michael.m.greenberg@gmail.com>
2016-10-24 11:00:38 -0700

    test harness running... only two failed on the first time. wow!

--- runtest.ml
+++ runtest.ml
@@ -1,9 +1,16 @@
-open Expansion
 open Test
 open Printf
 
-let tests = expansion_tests
 let main () =
-  printf ("...\n");printf ("Hello World!\n")
+  let failed = ref 0 in
+  print_endline "=== Running tests...";
+  List.iter
+    (fun t ->
+      match check_expansion t with
+      | Ok -> ()
+      | RErr(name,expected,got) ->
+         printf "%s failed: expected %s got %s\n" name "TODO" "derp";
+         incr failed)
+    expansion_tests
 
-let _ = main ()
+let _ = main ();;
