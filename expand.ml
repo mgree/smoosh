901d6b57ea9fadfd8a61e4a273d0502f6d446357
Michael Greenberg <michael.m.greenberg@gmail.com>
2017-06-19 15:14:10 -0700

    quasi-pretty printing in the expansion utility

--- expand.ml
+++ expand.ml
@@ -57,35 +57,49 @@ let parse_args () =
 (* AST munging ********************************************************)       
 (**********************************************************************)
 
+let show_unless expected actual =
+  if expected = actual
+  then ""
+  else string_of_int actual
+    
 let rec join (ws:words list) : words =
   match ws with
   | [] -> []
   | [w] -> w
   | w1::w2::ws -> w1 @ [F] @ join (w2::ws)
 
+let rec join_with (sep:words) (ws:words list) : words =
+  match ws with
+  | [] -> []
+  | [w] -> w
+  | w1::w2::ws -> w1 @ sep @ [F] @ join (w2::ws)
+                                  
 let join_map (f : 'a -> words) (l : 'a list) : words =
   join (List.map f l)
+
+let join_map_with (sep:words) (f : 'a -> words) (l : 'a list) : words =
+  join_with sep (List.map f l)
 
 let rec words_of_ast (e : Ast.t) : words =
   match e with
   | Command (_,assigns,cmds,redirs) ->
      join
-       [join_map (fun (_,arg) -> words_of_arg arg) assigns;
+       [join_map (fun (x,v) -> [S x;S "=";] @ words_of_arg v) assigns;
         join_map words_of_arg cmds;
         words_of_redirs redirs]
-  | Pipe (_,es) -> join_map words_of_ast es
+  | Pipe (_,es) -> join_map_with [S "|"]  words_of_ast es
   | Redir (_,e,redirs) -> join [words_of_ast e; words_of_redirs redirs]
-  | Background (_,e,redirs) -> join [words_of_ast e; words_of_redirs redirs]
-  | Subshell (_,e,redirs) -> join [words_of_ast e; words_of_redirs redirs]
-  | And (e1,e2) -> join [words_of_ast e1; words_of_ast e2]
-  | Or (e1,e2) -> join [words_of_ast e1; words_of_ast e2]
-  | Not e -> words_of_ast e
-  | Semi (e1,e2) -> join [words_of_ast e1; words_of_ast e2]
-  | If (e1,e2,e3) -> join [words_of_ast e1; words_of_ast e2; words_of_ast e3]
-  | While (e1,e2) -> join [words_of_ast e1; words_of_ast e2]
-  | For (_,arg,e,x) -> join [words_of_arg arg; words_of_ast e]
-  | Case (_,arg,cases) -> join [words_of_arg arg; words_of_cases cases]
-  | Defun (_,f,e) -> words_of_ast e
+  | Background (_,e,redirs) -> join [words_of_ast e; words_of_redirs redirs; [S "&"]]
+  | Subshell (_,e,redirs) -> join [[S "("]; words_of_ast e; words_of_redirs redirs; [S ")"]]
+  | And (e1,e2) -> join [words_of_ast e1; [S "&&"]; words_of_ast e2]
+  | Or (e1,e2) -> join [words_of_ast e1; [S "||"]; words_of_ast e2]
+  | Not e -> join [[S "!"]; words_of_ast e]
+  | Semi (e1,e2) -> join [words_of_ast e1; [S ";"]; words_of_ast e2]
+  | If (e1,e2,e3) -> join [[S "if"]; words_of_ast e1; [S "; then"]; words_of_ast e2; [S "; else"]; words_of_ast e3; [S "; fi"]]
+  | While (e1,e2) -> join [[S "while"]; words_of_ast e1; [S "; do"]; words_of_ast e2; [S "; done"]]
+  | For (_,arg,e,x) -> join [[S "for"]; [S x]; [S "in"]; words_of_arg arg; [S "; do"]; words_of_ast e; [S "; done"]]
+  | Case (_,arg,cases) -> join [[S "case"]; words_of_arg arg; [S "in"]; words_of_cases cases]
+  | Defun (_,f,e) -> join [[S (f ^ "()")]; words_of_ast e]
 and words_of_arg (a : Ast.arg) : words =
   List.map entry_of_arg_char a (* TODO string joining *)
 and entry_of_arg_char (ac : Ast.arg_char) : entry =
@@ -114,17 +128,27 @@ and entry_of_arg_char (ac : Ast.arg_char) : entry =
      K (Param (x,fmt))
   | Q a -> K (Quote (words_of_arg a))
   | B e -> K (Backtick (words_of_ast e))
+
 and words_of_redirs (rs : Ast.redirection list) : words =
   List.concat (List.map words_of_redir rs)
-and words_of_redir (r : Ast.redirection) : words =
-  match r with
-  | File (_,_,a) -> words_of_arg a
-  | Dup (_,_,_) -> []
-  | Heredoc (_,_,a) -> words_of_arg a
+              
+and words_of_redir = function
+  | File (To,fd,a)      -> S (show_unless 1 fd ^ ">") :: words_of_arg a
+  | File (Clobber,fd,a) -> S (show_unless 1 fd ^ ">|") :: words_of_arg a
+  | File (From,fd,a)    -> S (show_unless 0 fd ^ "<") :: words_of_arg a
+  | File (FromTo,fd,a)  -> S (show_unless 0 fd ^ "<>") :: words_of_arg a
+  | File (Append,fd,a)  -> S (show_unless 1 fd ^ ">>") :: words_of_arg a
+  | Dup (ToFD,fd,tgt)   -> [S (show_unless 1 fd ^ ">&" ^ string_of_int tgt)]
+  | Dup (FromFD,fd,tgt) -> [S (show_unless 0 fd ^ "<&" ^ string_of_int tgt)]
+  | Heredoc (t,fd,a) ->
+     (* compute an EOF marker *)
+     let heredoc = Ast.string_of_arg a in
+     let marker = Dash.fresh_marker (Dash.lines heredoc) "EOF" in
+     [S (show_unless 0 fd ^ "<<" ^ (if t = XHere then marker else "'" ^ marker ^ "'"))] @ words_of_arg a @ [S (marker ^ "\n")]
 and words_of_cases (cs : Ast.case list) : words =
   List.concat
-    (List.map (fun c -> words_of_arg c.Ast.cpattern @ words_of_ast c.Ast.cbody) cs)
-
+    (List.map (fun c -> words_of_arg c.Ast.cpattern @ [S ")"] @ words_of_ast c.Ast.cbody @ [S ";;"]) cs)
+                
 let rec expand_all (os : ty_os_state) (ws : words list) : ty_os_state * fields =
   match ws with
   | [] -> (os,[])
@@ -139,7 +163,7 @@ let main () =
   set_input_src ();
   let ns = Dash.parse_all () in
   let cs = List.map Ast.of_node ns in
-  let ws = List.map words_of_ast cs in
+  let ws = List.map words_of_ast cs in (* TODO restrict to only simple commands? *)
   let (os,fs) = expand_all !initial_os_state ws in
   List.iter (fun f -> print_endline f) fs;;
 
