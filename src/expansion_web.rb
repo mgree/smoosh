9f3ddaa44c831aa7ce41cc61ef856cbfd8edbb03
Michael Greenberg <michael.m.greenberg@gmail.com>
2017-09-20 11:12:11 -0700

    webbin

--- src/expansion_web.rb
+++ src/expansion_web.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'date'
 
 class ExpansionWeb < Sinatra::Base
   configure :development do
@@ -23,8 +24,34 @@ class ExpansionWeb < Sinatra::Base
   end
 
   post '/expand/submit' do
-    # write appropriate files with date/time/ip in the name + temp suffix (need code, env, user info)
+    # create temporary directory logging time and IP
+    timestamp = DateTime.now.to_s.gsub(":", "+")
+    ip = request.ip.gsub(":", "+") # just in case we're on the loop back interface    
+    d = Dir.mktmpdir "#{timestamp}_#{ip}" # TODO don't put this in var!
+
+    # create files with provided info
+    shell = File.new(File.join(d, "shell"), File::CREAT|File::TRUNC|File::WRONLY, 0644)
+    env   = File.new(File.join(d, "env"), File::CREAT|File::TRUNC|File::WRONLY, 0644)
+    user  = File.new(File.join(d, "users"), File::CREAT|File::TRUNC|File::WRONLY, 0644)
+
+    # write files
+    shell.write params['shell']
+    shell.close
+    
+    JSON.parse(params['env']).each do |k, v|
+      env.write "#{k}=#{v}\n"
+    end
+    env.close
+
+    JSON.parse(params['users']).each do |u, d|
+      user.write "#{u}=#{d}\n"
+    end
+    user.close
+    
+    "#{d} #{params['shell']} #{params['env']} #{params['users']}"
+    
     # run expander for JSON
+    #   /path/to/expand -env-file env -user-file users shell
     # catch error or send json out
   end
 end
