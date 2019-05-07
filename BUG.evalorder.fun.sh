99b35b59411c0ab19f3b58cb680d26c37957f937
Michael Greenberg <michael.greenberg@pomona.edu>
2019-05-07 09:02:40 -0400

    thought of a buggy behavior on the walk in to the cafe, will attend later [ci skip]

---
+++ BUG.evalorder.fun.sh
@@ -0,0 +1,9 @@
+show() { echo "got ${EFF-unset}"; }
+unset x
+EFF=${x=assign} show 2>${x=redir}
+[ -f assign ] && echo assign exists && rm assign
+[ -f redir ] && echo redir exists && rm redir
+
+# bash: assign
+# yash, dash: redir
+# smoosh: doesn't show up in the function at all!
