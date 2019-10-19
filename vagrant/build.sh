dd00a26f0ffc10633938949d48a23fa2b1d228c7
Michael Greenberg <michael.greenberg@pomona.edu>
2019-10-19 11:57:43 -0700

    working on vm build

---
+++ vagrant/build.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# camlp4 won't work in 4.08 right now 2019-06-18
+opam init
+opam switch 4.07.0
+
+eval `opam config env`
+
+echo ". /home/vagrant/.opam/opam-init/init.sh > /dev/null 2> /dev/null || true" >>~/.profile
+cat >~/.ocamlinit <<EOF
+let () =
+  try Topdirs.dir_directory (Sys.getenv "OCAML_TOPLEVEL_PATH")
+  with Not_found -> ()
+;;
+EOF
+
+
+# make sure we have ocamlfind and ocamlbuild
+opam install ocamlfind ocamlbuild
+
+# set up FFI for libdash; num library for lem; extunix for shell syscalls
+opam pin -n add ctypes 0.11.5
+opam install ctypes ctypes-foreign num extunix
+
+# build Oil... (disabled due to buggy libc test)
+# cd oil; ./configure; build/dev.sh minimal
+# sed -i 's#REPO_ROOT=.*#REPO_ROOT=/home/opam/oil#' /home/opam/oil/bin/osh
+# sudo ln -sf /home/opam/oil/bin/osh /usr/local/bin/osh
+
+cd smoosh
+
+# set up lem
+(cd lem/ocaml-lib; make install_dependencies)
+(cd lem; make; make install)
+PATH="/home/vagrant/smoosh/lem/bin:${PATH}"
+LEMLIB="/home/vagrant/smoosh/lem/library"
+export LEMLIB
+
+cat >>~/.profile <<EOF
+PATH="/home/vagrant/smoosh/lem/bin:${PATH}"
+LEMLIB="/home/vagrant/smoosh/lem/library"
+export LEMLIB
+EOF
+
+# build libdash, expose shared object
+(cd libdash; ./autogen.sh && ./configure --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu)
+(cd libdash; make && sudo make install)
+# build ocaml bindings
+(cd libdash/ocaml; make && make install)
+
+# build smoosh
+(cd src; make all)
+
+# install smoosh
+sudo cp src/smoosh /bin/smoosh
+
---
+++ vagrant/provision.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+echo "************************************************************************"
+echo "* INSTALLING DEPENDENCIES **********************************************"
+echo "************************************************************************"
+
+set -o errexit
+
+# silence apt-get
+DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND
+
+# we frontload installation so that things that inherit from us don't need network access
+# we also (inadvisedly) do an update, but we'll ask for particular shell versions
+apt-get update
+
+# other shells we'll want
+apt-get install -y dash=0.5.8-2.4
+apt-get install -y --no-install-recommends bash=4.4-5
+apt-get install -y yash=2.43-1
+apt-get install -y zsh=5.3.1-4+b3 && echo 'emulate sh' >~/.zshrc
+apt-get install -y ksh=93u+20120801-3.1
+apt-get install -y mksh=54-2+b4
+
+# for OSH
+#apt-get install -y python2.7 python python-dev time libreadline-dev
+
+# system support for libdash; libgmp for zarith for lem
+apt-get install -y autoconf autotools-dev libtool pkg-config libffi-dev libgmp-dev
+apt-get install -y opam
+
+echo "************************************************************************"
+echo "* BUILDING SMOOSH ******************************************************"
+echo "************************************************************************"
+
+su -c ". /home/vagrant/smoosh/vagrant/build.sh" vagrant
