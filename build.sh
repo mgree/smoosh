46c4ee142bf73e888cc510892f330f4d9f6a667f
Michael Greenberg <michael.greenberg@pomona.edu>
2020-02-18 19:51:15 -0800

    better version strings

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+make -C src bump version.lem && \
 docker build -t smoosh . && \
 sleep 1 && \
 docker build -t smoosh-test -f Dockerfile.test .
--- src/mk_version.sh
+++ src/mk_version.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-build=$(date "+%Y-%m-%d_%H:%M")
+time=$(date "+%Y-%m-%d %H:%M")
+build=$(git describe --always)
 
 cat <<EOF
 open import Pervasives_extra
@@ -13,6 +14,9 @@ let smoosh_version = "0.1"
 val smoosh_build : string
 let smoosh_build = "$build"
 
+val smoosh_time : string
+let smoosh_time = "$time"
+
 val smoosh_info : string
-let smoosh_info = "smoosh v" ^ smoosh_version ^ " (build " ^ smoosh_build ^ ")\n"
+let smoosh_info = "smoosh v" ^ smoosh_version ^ " (build " ^ smoosh_build ^ " on " ^ smoosh_time ^ ")\n"
 EOF
