c963e8c59d63798adc4b8ad0e946a473c394a669
Michael Greenberg <michael.greenberg@pomona.edu>
2020-05-13 22:03:10 -0700

    use the correct path a hur hur hur

--- web/src/mk_config.sh
+++ web/src/mk_config.sh
@@ -1,14 +1,19 @@
+#!/bin/sh
+
+BASE="$1"
+: ${BASE:=$HOME/smoosh}
+
 cat <<EOF
 development:
-  shtepper_executable: $HOME/src/shtepper
-  submissions_tmpdir: $HOME/web/submissions
+  shtepper_executable: $BASE/src/shtepper
+  submissions_tmpdir: $BASE/web/submissions
   bind: 0.0.0.0
 test:
-  shtepper_executable: $HOME/src/shtepper
-  submissions_tmpdir: $HOME/web/submissions
+  shtepper_executable: $BASE/src/shtepper
+  submissions_tmpdir: $BASE/web/submissions
   bind: 0.0.0.0
 production:
-  shtepper_executable: $HOME/src/shtepper
-  submissions_tmpdir: $HOME/web/submissions
+  shtepper_executable: $BASE/src/shtepper
+  submissions_tmpdir: $BASE/web/submissions
   bind: 0.0.0.0
 EOF
