eddd1c9da1c990076238ad58f423ddce3266b047
ishaangandhi <ishaangandhi@gmail.com>
2020-09-10 22:06:42 -0400

    Can translate all but heredocs
    Can convert Morsmall AST to internal AST for all cases except Heredocs.
    
    There are a few different bugs in play as well.

---
+++ src/morbig_shim.ml
@@ -0,0 +1,191 @@
+open Morbig
+open Smoosh_prelude
+
+exception ParseException of string
+
+let rec parse_program (program : Morsmall.AST.program) : Smoosh_prelude.stmt =
+  match program with
+  | [] -> failwith "No program"
+  | _ :: _ :: _ -> failwith "Can only handle single program"
+  | [ cmd ] -> parse_command cmd
+
+and morsmall_word_to_smoosh_entry ({ value; position } : Morsmall.AST.word') :
+    Smoosh_prelude.entry =
+  let entries = morsmall_wordval_to_smoosh_entries value in
+  assert (List.length entries = 1);
+  List.hd entries
+
+and morsmall_word_to_smoosh_entries ({ value; position } : Morsmall.AST.word') :
+    Smoosh_prelude.entry list =
+  let entries = morsmall_wordval_to_smoosh_entries value in
+  entries
+
+and morsmall_words_to_smoosh_entries (words : Morsmall.AST.word' list) :
+    Smoosh_prelude.entry list =
+  let entries_lists = List.map morsmall_word_to_smoosh_entries words in
+  let entries = List.flatten entries_lists in
+  separate_strings entries
+
+(* and morsmall_wordvals_to_smoosh_entry (words : Morsmall.AST.word list) :
+    Smoosh_prelude.entry =
+  let entries_lists = List.map morsmall_word_to_smoosh_entries words in
+  let entries = List.flatten entries_lists in
+  assert (List.length entries = 1);
+  List.hd entries *)
+
+(* CASE ITEMS *)
+and morsmall_wordvals_to_smoosh_words_list (words : Morsmall.AST.word list) :
+    Smoosh_prelude.words list =
+  let entries_lists = List.map morsmall_wordval_to_smoosh_entries_no_sep words in
+  let rec flatten_strings entry_list = 
+    match entry_list with
+    S s1 :: S s2 :: r -> flatten_strings @@ S (s1 ^ s2) :: r
+    | _ :: r -> (List.hd entry_list) :: flatten_strings r
+    | _ -> entry_list
+  in
+  (* let entries = List.flatten entries_lists in *)
+  List.map flatten_strings entries_lists
+
+
+and morsmall_wordvals_to_smoosh_entries (words : Morsmall.AST.word list) :
+    Smoosh_prelude.entry list =
+  let entries_lists = List.map morsmall_wordval_to_smoosh_entries words in
+  let entries = List.flatten entries_lists in
+  separate_strings entries
+
+(* TODO : Find mapping to smoosh_formats *)
+and morsmall_attribute_to_smoosh_format (attr : Morsmall.AST.attribute) =
+  match attr with
+  | Morsmall.AST.NoAttribute -> Normal
+  | Morsmall.AST.ParameterLength _ -> Normal
+  | Morsmall.AST.UseDefaultValues _ -> Normal
+  | Morsmall.AST.AssignDefaultValues _ -> Normal
+  | Morsmall.AST.IndicateErrorifNullorUnset _ -> Normal
+  | Morsmall.AST.UseAlternativeValue _ -> Normal
+  | Morsmall.AST.RemoveSmallestSuffixPattern _ -> Normal
+  | Morsmall.AST.RemoveLargestSuffixPattern _ -> Normal
+  | Morsmall.AST.RemoveSmallestPrefixPattern _ -> Normal
+  | Morsmall.AST.RemoveLargestPrefixPattern _ -> Normal
+
+and morsmall_wordval_to_smoosh_entries (w : Morsmall.AST.word) :
+    Smoosh_prelude.entry list =
+separate_strings @@ morsmall_wordval_to_smoosh_entries_no_sep w
+
+and morsmall_wordval_to_smoosh_entries_no_sep (w : Morsmall.AST.word) :
+    Smoosh_prelude.entry list =
+  let wc_to_substr (wc : Morsmall.AST.word_component) : Smoosh_prelude.entry =
+    match wc with
+    | Morsmall.AST.Literal s -> if s = "$((0-5))" then print_endline "literal"; S s
+    | Morsmall.AST.DoubleQuoted w -> K (Quote ([], morsmall_wordval_to_smoosh_entries w))
+    | Morsmall.AST.Variable (name, attribute) ->
+        K (Param (name, morsmall_attribute_to_smoosh_format attribute))
+    | Morsmall.AST.Subshell p -> K (Backtick (parse_program p))
+    | Morsmall.AST.GlobAll -> S "*"
+    | Morsmall.AST.GlobAny -> S "."
+    | Morsmall.AST.BracketExpression exp -> S "<BracketExpression>"
+  in
+  List.map wc_to_substr w
+
+(* If 2 S's in a row, insert F between them? *)
+and separate_strings str_list =
+  match str_list with
+  | S s1 :: S s2 :: r -> S s1 :: F :: separate_strings (S s2 :: r)
+  | S s :: K c :: r -> S s :: F :: separate_strings (K c :: r)
+  | K c :: S s :: r -> K c :: F :: separate_strings (S s :: r)
+  | K c1 :: K c2 :: r -> K c1 :: F :: separate_strings (K c2 :: r)
+  | _ :: r -> (List.hd str_list) :: separate_strings r
+  | [] -> []
+
+and parse_command ({ value; position } : Morsmall.AST.command') :
+    Smoosh_prelude.stmt =
+  match value with
+  | Morsmall.AST.Simple (assignmentList, words) ->
+      let command_opts =
+        {
+          ran_cmd_subst = false;
+          should_fork = false;
+          force_simple_command = false;
+        }
+      in
+      let morsmall_to_smoosh_assignment
+          ({ value; position } : Morsmall.AST.assignment') =
+        let aName, aWord = value in
+        (aName, morsmall_wordval_to_smoosh_entries aWord)
+      in
+      let assignments = List.map morsmall_to_smoosh_assignment assignmentList in
+      let args = morsmall_words_to_smoosh_entries words in
+      Command (assignments, args, [], command_opts)
+  | Morsmall.AST.Async cmd -> Done
+  | Morsmall.AST.Seq (cmd1, cmd2) ->
+      Semi (parse_command cmd1, parse_command cmd2)
+  | Morsmall.AST.And (cmd1, cmd2) -> And (parse_command cmd1, parse_command cmd2)
+  | Morsmall.AST.Or (cmd1, cmd2) -> Or (parse_command cmd1, parse_command cmd2)
+  | Morsmall.AST.Not cmd -> Not (parse_command cmd)
+  | Morsmall.AST.Pipe (cmd1, cmd2) ->
+    let rec collect_piped_commands (c : Morsmall.AST.command') = 
+      match c.value with 
+      | Morsmall.AST.Pipe (c1, c2) -> collect_piped_commands c1 @ [c2]
+      | _ -> [c] in
+    let left_stmts = List.map parse_command (collect_piped_commands cmd1) in
+    let right_stmt = parse_command cmd2 in
+      Pipe (FG, left_stmts @ [right_stmt])
+      (* TODO: ishaangandhi, All pipes are FG. When should we make them background *)
+  | Morsmall.AST.Subshell cmd ->
+      let redir_state = ([], None, []) in
+      Subshell (parse_command cmd, redir_state)
+  | Morsmall.AST.For (x, listOpt, c) -> (
+      match listOpt with
+      | None -> failwith "Empty list in for?"
+      | Some l ->
+          For (x, morsmall_wordvals_to_smoosh_entries l, parse_command c)
+      )
+  | Morsmall.AST.Case (var, cases) ->
+      let morsmall_to_smoosh_case_item ({ value; _ } : Morsmall.AST.case_item')
+          =
+        let wl, cmdOpt = value in
+        let smoosh_cmd =
+          match cmdOpt with None -> Done | Some cmd -> parse_command cmd
+        in
+        let smoosh_wl =
+            (* List.iter (fun x -> print_endline @@ Morsmall.AST.show_word x) wl.value; *)
+          morsmall_wordvals_to_smoosh_words_list wl.value
+        in
+        (smoosh_wl, smoosh_cmd)
+      in
+      let smoosh_words = morsmall_wordval_to_smoosh_entries var in
+      let smoosh_case_items = List.map morsmall_to_smoosh_case_item cases in
+      Case (smoosh_words, smoosh_case_items)
+  (* execute c1 and use its exit status to determine whether to execute c2 or c3.
+     In fact, c3 is not mandatory and is thus an option. *)
+  | Morsmall.AST.If (c1, c2, c3) ->
+      let else_stmt =
+        match c3 with None -> Done | Some c3val -> parse_command c3val
+      in
+      If (parse_command c1, parse_command c2, else_stmt)
+  (* The while Loop. While (c1, c2) shall continuously execute c2 as long as c1
+     has a zero exit status. *)
+  | Morsmall.AST.While (c1, c2) -> While (parse_command c1, parse_command c2)
+  (* The until Loop. Until (c1, c2) shall continuously execute c2 as long as c1
+     has a non-zero exit status. *)
+  | Morsmall.AST.Until (c1, c2) ->
+      While (Not (parse_command c1), parse_command c2)
+  (* A function is a user-defined name that is used as a simple command to call
+     a compound command with new positional parameters. A function is defined with a
+     function definition command, Function (name, body). *)
+  (* This function definition command defines a function named name: string
+     and with body body: command. The body shall be executed whenever name is
+     specified as the name of a simple command. *)
+  | Morsmall.AST.Function (name, body) -> Defun (name, parse_command body)
+  | Morsmall.AST.Redirection (c, desc, kind, w) -> 
+      let redir_state = ([], None, []) in
+      Redir (parse_command c, redir_state)
+  | Morsmall.AST.HereDocument (_, _, _) -> Done
+
+let parse_string_morbig (cmd : string) : Smoosh_prelude.stmt =
+  let ast =
+    Morsmall.CST_to_AST.program__to__program
+    @@ Morbig.parse_string "parse_string_morbig" cmd
+  in
+  (* print_endline @@ Morsmall.AST.show_program ast; *)
+  parse_program ast
+  
---
+++ src/test_morbig.ml
@@ -0,0 +1,80 @@
+open Test_prelude
+open Smoosh
+open Os_symbolic
+open Path
+open Printf
+
+(***********************************************************************)
+(* DRIVER **************************************************************)
+(* Test that Morbig generates the same AST as Dash *********************)
+(***********************************************************************)
+
+(***********************************************************************)
+(* Incremental parsing *************************************************)
+(* Protocol: 
+ *
+ *   parse_init (parse_next* parse_done)
+ *
+ * ParseDone and ParseError mean you're done, and should stop calling parse_next.
+ * ParseNull represents an empty line. ParseStmt is a successfully parsed line.
+ *
+ * The parse_string function is a convenience for testing. parse_done will be
+ * called for you when you evaluate the resulting command.
+ *
+ * See smoosh.lem for the definition of parse_source and parse_result.
+ *
+ * It's critical that set_ps1 and set_ps2 be called whenever those prompts are 
+ * updated---parsers do the prompting themselves.
+ *
+ * ??? It may or may not be a problem to call parse_done before you're done.
+ *
+ * ??? 2018-11-14 interrupts during parsing? hopefully handled after any forking?
+ *
+ *)
+
+let dash_parsed_str cmd =
+  Shim.parse_init (ParseString (ParseEval, cmd));
+  let rec build_result () =
+    let next_parsed = Shim.parse_next Noninteractive in
+    match next_parsed with
+    | Smoosh_prelude.ParseDone -> []
+    | Smoosh_prelude.ParseError _ -> []
+    | Smoosh_prelude.ParseNull -> build_result ()
+    | Smoosh_prelude.ParseStmt c -> c :: build_result ()
+  in
+  build_result ()
+
+let test_programs =
+  let test_strs = 
+  (List.map (fun (s, _, _) -> s) Test_evaluation.exit_code_tests) @
+  ["true | true | true"]
+  in
+  List.map
+    (fun s -> (s, List.hd @@ dash_parsed_str s))
+    (test_strs @ ["s"])
+
+let json_str_of_stmt s =
+  let b = Buffer.create 0 in
+  let js = Shim.json_of_stmt s in
+  Shim.write_json b js;
+  Buffer.contents b
+
+let check_tree_equivalence (cmd, expected) =
+  checker Smoosh.parse_string_morbig
+    (fun s1 s2 -> json_str_of_stmt s1 = json_str_of_stmt s2)
+    (cmd, cmd, expected)
+
+let run_tests () =
+  let failed = ref 0 in
+  let test_count = ref 0 in
+  print_endline "\n=== Initializing Dash parser...";
+  Dash.initialize ();
+  print_endline "=== Running Morbig tests...";
+  test_part "Morbig" check_tree_equivalence json_str_of_stmt test_programs
+    (* test_part "Morbig" check_tree_equivalence Smoosh_prelude.string_of_stmt test_programs *)
+    test_count failed;
+  printf "=== ...ran %d Morbig tests with %d failures.\n\n" !test_count !failed;
+  !failed = 0
+
+;;
+run_tests ()
